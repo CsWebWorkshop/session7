ec1cb1bbfa33cae51141abfe331761181021965f
moeid <moeid.kh1381@gmail.com>
2023-05-03 19:15:02 +0430

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$request = $_SERVER['REQUEST_URI'];
+$request = explode('?', $request)[0];
+
+if($request == '' || $request == '/') {
+    require './pages/main.php';
+}
+elseif (preg_match('/^\/products(\/)/', $request)) {
+    $request = str_replace('/products', '', $request);
+    $part = explode('/', $request);
+    $productID = $part[1];
+    if (preg_match('/^\/[0-9]+$/', $request)) {
+        require './pages/details.php';
+    } elseif (preg_match('/^\/[0-9]+\/Update$/', $request)) {
+        echo $request;
+    }
+}
+?>
